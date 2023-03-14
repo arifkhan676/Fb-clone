b84d7f19d34d3071d6ffa3d1f7c9b73c62ecc491
arifkhan676 <arifulhasan5500@gmail.com>
2023-03-14 21:52:09 +0600

    js div fixed for post

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const textIn=document.getElementById("textInp");
 
-console.log(textIn);
-
 const bt=document.getElementById("btPost");
 
 const pid=document.getElementById("pId");
@@ -11,8 +9,26 @@ const secId = document.getElementById("spId");
 bt.addEventListener('click',() => {
 
    pid.textContent=textIn.value;
-   secId.innerHTML=new Date();
+   secId.innerHTML=new Date().toLocaleTimeString();
 
 })
-document.getElementById("root").appendChild(pid);
-document.getElementById("root").appendChild(secId);
+
+
+const x = document.createElement('div');
+x.id="newPost2";
+
+const t = document.getElementById("postDiv2").appendChild(x);
+
+
+document.getElementById("newPost2").appendChild(pid);
+document.getElementById("newPost2").appendChild(secId);
+
+//console.log(t);
+
+//console.log(t);
+
+/*
+
+
+
+*/
