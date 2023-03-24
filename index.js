0ace214014b1f095f01b03beb3ed16d189acc0b6
arifkhan676 <arifulhasan5500@gmail.com>
2023-03-24 15:37:48 +0600

    dropdown

--- index.js
+++ index.js
@@ -49,6 +49,19 @@ function gotoInput(){
 }
 
 
+
+
+/*
+popup
+
+<div class="popup" id="popup" >
+  <img src="images/tick.png" alt="">
+   <h2>Thank You!</h2>
+   <p>Your information has successfully submitted!</p>
+   <button type="submit"  class="btn btn-outline-dark" onClick="closePopup()">Ok</button> 
+ </div>
+</div>
+
 let popup=document.getElementById("popup");
 function openPopup(){
  popup.classList.add("open-popup");
@@ -57,58 +70,7 @@ function closePopup(){
  popup.classList.remove("open-popup");
 }
 
+addEventLister learning***
+ */
 
 
-
-
-/*
-function gotoInput(){
-   const input = document.createElement('input');
-   input.type="file";
-   input.id="myInputId";
-   input.accept="image/png, image/jpg ,image/jpeg";
-   document.getElementById("postDiv1").appendChild(input);
-} */
-
-
-
-
-
-/* addEventLister learning***
-
-
-   const img = document.createElement('img');
-   img.classList.add('postImg');
-   img.id="profile-pic"
-   img.src = "images/profile.png";
-   
-   var takePic = document.getElementById("profile-pic");
-   var fileInput = document.getElementById("myInputId");
-   fileInput.onchange=function (){
-   takePic.src = URL.createObjectURL(fileInput.files[0]);
- }
-
-
-
-              <img class="xImg" src="images/profile.png" width="300px" height="300px" id="profile-pic"  alt="">
-              <input  type="file" id="myInputId"  accept="image/png, image/jpg ,image/jpeg">
-
-
-
-           <button class="btn-pro" onclick="uploadFile()"> <i class="fa-solid fa-image"></i>  </button> 
-
-
-
- var file = fileInput.files[0]; //koto gula pic nicchi array te insert hibe
-   var formData = new FormData();  //FormData oi pic or value gulo pair akare represnt korbe & XMLHttpRequest() diye 
-   formData.append("file",file); // file append kortese
-   var xhr = new XMLHttpRequest(); // XMLHttpRequest() diye data retrieve korbe URL diye page theke
-   xhr.open("POST","upload.php",true); //pic nicche r boolean diye test krtese
-   xhr.onreadystatechange = function(){
-      if(xhr.readyState === 4 && xhr.status === 200 ){
-         console.log(xhr.responseText);
-      }
-   };
-   xhr.send(formData);
-
-*/
